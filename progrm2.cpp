7378342d66d8648418fffd484f66ef6d04045c27
rajendra098 <101593905+rajendra098@users.noreply.github.com>
2022-03-15 00:21:07 +0545

    Add files via upload

---
+++ progrm2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    system("cls");
+    float number1, number2, sum, average;
+    cout<<"Enter two numbers:";
+    cin >> number1;
+    cin >> number2;
+}
