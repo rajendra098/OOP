fe95498c04242af96e47e637b8fc1bad0ed9d1b6
Rajendra Joshi <101593905+rajendra098@users.noreply.github.com>
2022-03-17 17:55:42 +0545

    Create sum_of_two_number.cpp

---
+++ sum_of_two_number.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    system("cls");
+    float number1, number2, sum, average;
+    cout<<"Enter two numbers:";
+    cin >> number1;
+    cin >> number2;
+    sum=number1+number2;
+    cout<<"The sum of two number is"<<sum<<endl;
+}
